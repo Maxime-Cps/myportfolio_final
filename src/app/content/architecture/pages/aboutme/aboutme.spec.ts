ba2aa83d43b97e0d04190549ce20d1ab05e17a27
Maxime Chapuis <maxime.chapuis@etu.unistra.fr>
2026-05-23 08:18:48 +0200

    fix: provide HttpClient in Aboutme and Projects specs

--- src/app/content/architecture/pages/aboutme/aboutme.spec.ts
+++ src/app/content/architecture/pages/aboutme/aboutme.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { Aboutme } from './aboutme';
 
@@ -8,7 +10,11 @@ describe('Aboutme', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Aboutme]
+      imports: [Aboutme],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     })
     .compileComponents();
 
