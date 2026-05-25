e0a7c30836fd0b4d67e07bb9e07e6ed6ffc2fad2
Maxime Chapuis <maxime.chapuis@etu.unistra.fr>
2026-05-25 11:17:55 +0200

    fix: provide HttpClient in Experiences spec

--- src/app/content/architecture/pages/experiences/experiences.spec.ts
+++ src/app/content/architecture/pages/experiences/experiences.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { Experiences } from './experiences';
 
@@ -8,7 +10,8 @@ describe('Experiences', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Experiences]
+      imports: [Experiences],
+      providers: [provideHttpClient(), provideHttpClientTesting()]
     })
     .compileComponents();
 
