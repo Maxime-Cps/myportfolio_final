2a0bf657b249f6b2e9c2e7bee7040c18457616ba
Maxime Chapuis <maxime.chapuis@etu.unistra.fr>
2026-06-26 16:49:12 +0200

    optimize perf

--- src/app/content/architecture/body/body.ts
+++ src/app/content/architecture/body/body.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {NeatComponent} from '../../components/neat/neat';
 @Component({
   selector: 'app-body',
   imports: [
     RouterOutlet,
-    NeatComponent,
   ],
   templateUrl: './body.html',
   styleUrl: './body.scss'
